36aca509ee6bb90f4780d6ff8770f5980a192f97
vineetmidha <50925587+vineetmidha@users.noreply.github.com>
2020-09-21 15:13:25 +0530

    Create CP_02.cpp
    2.	Give a string containing a variable name in camel case, decipher the same and write each word on a different line.
    
    String s = IAmACompetitiveProgrammer
    I
    Am
    A
    Competitive
    Programmer

---
+++ CP_02.cpp
@@ -0,0 +1,37 @@
+'''
+2.	Give a string containing a variable name in camel case, decipher the same and write each word on a different line.
+
+String s = IAmACompetitiveProgrammer
+I
+Am
+A
+Competitive
+Programmer
+'''
+
+#include <iostream>
+#include <cctype>
+using namespace std;
+
+void solve(string s){
+    cout<<s.at(0);
+    for (int i=1; i<s.length(); i++){
+        if (isupper(s.at(i)))
+            cout<<endl;
+        cout<<s.at(i);
+    }
+}
+int main() {
+    string s = "IAmACompetitiveProgrammer";
+    solve(s);
+}
+
+'''
+Output:
+
+I
+Am
+A
+Competitive
+Programmer
+'''
