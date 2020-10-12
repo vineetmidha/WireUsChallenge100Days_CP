4a545173d2feb2bdf05b2f63a0df21e1606b98ab
vineetmidha <50925587+vineetmidha@users.noreply.github.com>
2020-10-12 14:58:01 +0530

    Create CP_009.cpp
    9.	Given an array with n values, reverse the array. 
    
    Input: 
    4
    1 2 4 3
    Output:
    3 4 2 1

---
+++ CP_009.cpp
@@ -0,0 +1,44 @@
+// Problem statement in description
+
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+void reverse(int a[], int n){
+    for (int i=0; i<n/2; i++){
+        int t = a[i];
+        a[i] = a[n-1-i];
+        a[n-1-i] = t;
+    }
+}
+
+int main() {
+    int t, n;
+    
+    cin >> n;
+    
+    int a[n];
+    
+    for (int i=0; i<n; i++){
+        cin >> a[i];
+    }
+
+    reverse(a, n);
+    
+    for (int i=0; i<n; i++){
+        cout << a[i] << " ";
+    }
+    
+	return 0;
+}
+
+/*
+
+Input: 
+4
+1 2 3 4 5
+Output:
+5 4 3 2 1
+
+*/
